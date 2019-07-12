5d6171b64b5f1b63a014cfceb441c54012a9bcd2
Vadim Lisakovich <vadim.lisakovich@dfsco.com>
2019-07-12 14:08:35 +0300

    added index.ts

---
+++ src/app/index.ts
@@ -0,0 +1,5 @@
+export {
+  SharedFeatureComponent
+} from './samp/components/shared-feature/shared-feature.component';
+export { MathService } from './samp/Services/math.service';
+export { SimpModModule } from './samp/samp.module';
